93ff2a2c1309a935e5d47d9e8f91eddb44bde6ed
Paul-Marte <jmarte01@mynbcc.ca>
2025-03-05 21:28:32 -0400

    Event Controller Created

---
+++ Restaurant/src/main/java/nbcc/restaurant/controllers/EventController.java
@@ -0,0 +1,22 @@
+package nbcc.restaurant.controllers;
+
+import jakarta.validation.Valid;
+import nbcc.restaurant.entities.Event;
+import nbcc.restaurant.repositories.EventRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class EventController {
+
+    private final EventRepository eventRepo;
+
+    public EventController(EventRepository eventRepo) {
+        this.eventRepo = eventRepo;
+    }
+
+
+}

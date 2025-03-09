d18089c7a86a23d6dc72aed610d6e33f8f14dcf6
Paul-Marte <jmarte01@mynbcc.ca>
2025-03-08 22:26:40 -0400

    Event - Detail get method in event controller Event Detail View

--- Restaurant/src/main/java/nbcc/restaurant/controllers/EventController.java
+++ Restaurant/src/main/java/nbcc/restaurant/controllers/EventController.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Objects;
 
 @Controller
 public class EventController {
@@ -36,17 +39,32 @@ public class EventController {
     @PostMapping({ "/event/create"})
     public String create(@Valid Event event, BindingResult bindingResult, Model model){
 
+
+        if(bindingResult.hasErrors()){
+            return "/events/create";
+        }
+
         if(event.getStartDate().isAfter(event.getEndDate())){
             bindingResult.rejectValue("startDate", "error.startDate", "Start date cannot be after end date");
-            //return "/events/create";
         }
 
         if(bindingResult.hasErrors()){
             return "/events/create";
         }
 
-
         eventRepo.save(event);
         return "redirect:/events";
     }
+
+    @GetMapping({ "/event/{id}"})
+    public String detail(Model model, @PathVariable long id){
+        var entity= eventRepo.findById(id);
+
+        if(entity.isPresent()){
+            model.addAttribute("event", entity.get());
+            return "/events/detail";
+        }
+        return "redirect:/events";
+    }
+
 }

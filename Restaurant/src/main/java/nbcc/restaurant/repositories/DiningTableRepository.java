ad4261047753d919d2ef8f7de695ba13e6acbbfc
ArielZhaoLiu <zliu03@mynbcc.com>
2025-03-08 23:09:25 -0400

    Create DiningTable Repository

---
+++ Restaurant/src/main/java/nbcc/restaurant/repositories/DiningTableRepository.java
@@ -0,0 +1,7 @@
+package nbcc.restaurant.repositories;
+
+import nbcc.restaurant.entities.DiningTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiningTableRepository extends JpaRepository<DiningTable, Long> {
+}

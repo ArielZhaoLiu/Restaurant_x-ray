3108e2f80e7feaac7d3b7d6843097baf67a3cb29
ArielZhaoLiu <zliu03@mynbcc.com>
2025-03-08 23:32:24 -0400

    For Edit Layout, update LayoutController GetMapping, create editLayout html and add link to editLayout

--- Restaurant/src/main/java/nbcc/restaurant/controllers/LayoutController.java
+++ Restaurant/src/main/java/nbcc/restaurant/controllers/LayoutController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -39,6 +40,17 @@ public class LayoutController {
             return "/layouts/create";
         }
         layoutRepo.save(layout);
+        return "redirect:/layouts";
+    }
+
+    @GetMapping("/layout/edit/{id}")
+    public String edit(Model model, @PathVariable long id) {
+        var entity = layoutRepo.findById(id);
+
+        if(entity.isPresent()) { // this means the entity was found in the database
+            model.addAttribute("layout", entity.get());
+            return "/layouts/edit";
+        }
         return "redirect:/layouts";
     }
 }

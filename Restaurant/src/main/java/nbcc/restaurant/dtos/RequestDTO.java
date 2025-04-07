795b8806a60cfc5cba00645f528adf5fc51b8bfb
Paul-Marte <jmarte01@mynbcc.ca>
2025-04-07 08:19:44 -0300

    RequestDTO

---
+++ Restaurant/src/main/java/nbcc/restaurant/dtos/RequestDTO.java
@@ -0,0 +1,89 @@
+package nbcc.restaurant.dtos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import nbcc.restaurant.entities.ReservationStatus;
+import nbcc.restaurant.entities.Seating;
+
+public class RequestDTO {
+
+
+    private long id;
+    private long seating_id;
+    private long event_id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int groupSize;
+
+    public RequestDTO() {
+    }
+
+    public RequestDTO(long id, long seating_id, long event_id, String firstName, String lastName, String email, int groupSize) {
+        this.id = id;
+        this.seating_id = seating_id;
+        this.event_id = event_id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.groupSize = groupSize;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getSeating_id() {
+        return seating_id;
+    }
+
+    public void setSeating_id(long seating_id) {
+        this.seating_id = seating_id;
+    }
+
+    public long getEvent_id() {
+        return event_id;
+    }
+
+    public void setEvent_id(long event_id) {
+        this.event_id = event_id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getGroupSize() {
+        return groupSize;
+    }
+
+    public void setGroupSize(int groupSize) {
+        this.groupSize = groupSize;
+    }
+}
